ad909e263c0274ea075b540ac73c19d5ce94b1b8
anjeljayswal <anjeljayswal@gmail.com>
2023-11-09 18:44:13 +0530

    image came to single blog and edit form

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ require("./model/index.js")
 // say nodejs that we are using ejs, set everything
 app.set("view engine","ejs")
 
+const fs = require("fs");
 
 // telling nodejs to accept the incoming data(parsing data)
 app.use(express.json()) // cT = application/json handle
@@ -87,13 +88,93 @@ app.post("/addBlog",upload.single('image'), async(req,res)=>{
     title , 
     subTitle  ,
     description ,
-    imageUrl : req.file.filename
+    imageUrl : process.env.BACKEND_URL + req.file.filename
    })
 //    res.send("BLog created successfully")
    res.redirect("/")
 
 })
+//edit blog form
+app.get("/edit/:id",async(req,res)=>{
+    // res.render("editBlog")
+    // find the blog with coming id 
+    const id = req.params.id 
+    const blog= await blogs.findAll({
+        where :{
+            id:id
+        }
+    })
+    res.render("editBlog.ejs",{blog:blog})
+})
 
+
+// edit form bata aako data handle 
+app.post("/edit/:id", upload.single('image'),async(req,res)=>{
+
+    const id = req.params.id;
+    const { title, subTitle, description} = req.body;
+    let fileName 
+    if(req.file){
+        fileName = req.file.fieldname
+    }
+
+    // fs file system package, unlink method ho 
+    // fs.unlink("./uploads/test.txt",(err)=>{
+    //     if(err){
+
+
+    //     }else{
+    //         console.log("File delet succefully");
+    //     }
+    // })
+    
+    //old data
+    const oldData = await blogs.findAll({
+        where:{
+            id:id
+        }
+    })
+    const oldFileName = oldData[0].imageUrl
+    // console.log(oldFileName)
+    const lengthToCut = "http://localhost:3000/".length
+    // console.log(lengthToCut)
+
+    const oldFileNameAfterCut = oldFileName.slice(lengthToCut);
+    // console.log(oldFileNameAfterCut)
+    
+    // return
+    
+
+    if(fileName){
+        //delete old beacuase naya aairako xa
+        fs.unlink("./uploads/" + oldFileNameAfterCut,(err)=>{
+            if(err){
+                console.log("error occured", err)
+            }else{
+                console.log("old file delete successfully");
+            }
+        })
+    }
+
+    // to keep in dbms 
+    await blogs.update({
+        title:title,
+        subTitle:subTitle,
+        description:description,
+        imageUrl : fileName ? process.env.BACKEND_URL + fileName : oldFileName
+    },{
+        where:{
+            id:id
+        }
+    })
+
+    res.redirect("/blog/"+id)
+})
+
+// //edit form bata aako data handle
+// app.post("/edit/:id",(res,req)=>{
+//     console.log(req.body)
+// })
 
 
 // Function to format a date string
